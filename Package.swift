e922fa5d8c4c2f51288826691f22bb031d075f09
Marcos felipe <mfelipesp@gmail.com>
2020-05-27 16:39:31 -0300

    new version

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MFSwiftCommom",            
-            platforms: [.iOS(.v13)],
+            // platforms: [.iOS(.v13)],
             targets: ["MFSwiftCommom"]),
     ],
     dependencies: [
